6ae4cb467993c067b27c1cddfa74e780a82635fd
BillyNgEng <symmetrictensor@gmail.com>
2022-12-15 18:07:41 +0800

    modification regarding the login page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
 import './App.css';
 import { BrowserRouter, Route, Routes} from "react-router-dom";
-
 import Login from './Login';
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./Login.css";
+
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function validateForm() {return email.length > 0 && password.length > 0;}
+  function handleSubmit(event) { event.preventDefault();}
+
   return (
     // Login page
     // User page
     // Admin page
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email </Form.Label>
+          <Form.Control autoFocus type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password </Form.Label>
+          <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>Login</Button>
+      </Form>
 
     <BrowserRouter>
       <div>
-        <ul>
-          <li><Link to='/'>Login</Link></li>
-          <li><Link to='/user'>User page</Link></li>
-          <li><Link to='/admin'>Admin page</Link></li>
-        </ul>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path='/user' element={<User/>} />
-          <Route path='/admin' element={<Admin/>} />
         </Routes>
       </div>
     </BrowserRouter>
-  );
+  </div>
+
+);
+
 }
+
 
 export default App;
