0e4e48851b67cc8dd87ede3bd01daeee773f484c
BillyNgEng <symmetrictensor@gmail.com>
2022-12-15 16:41:23 +0800

    Update App.js
    Route and link updated

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ function App() {
     // Login page
     // User page
     // Admin page
-<BrowserRouter>
+
+    <BrowserRouter>
       <div>
+        <ul>
+          <li><Link to='/'>Login</Link></li>
+          <li><Link to='/user'>User page</Link></li>
+          <li><Link to='/admin'>Admin page</Link></li>
+        </ul>
         <Routes>
           <Route path="/" element={<Login />} />
-          
+          <Route path='/user' element={<User/>} />
+          <Route path='/admin' element={<Admin/>} />
         </Routes>
       </div>
     </BrowserRouter>
