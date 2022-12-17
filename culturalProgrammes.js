5d33f6cbc60eaf808799867fe2f32c5003853da0
Essene21a <110793490+Essene21a@users.noreply.github.com>
2022-12-17 11:00:05 +0800

    Add files via upload

---
+++ culturalProgrammes.js
@@ -0,0 +1,24 @@
+export const data = [
+
+	{
+		id: 1,
+		title:  "'A Woman Is A Woman'- Fest of Belles and Queens: Actresses European and Hollywood",
+		venue: "8263",
+		data: "11 December 2022 (Sun) 11:30am 13 January 2023 (Fri) 8pm",
+		time: "84 mins",
+		description:"ive...",
+		presenter:"Presented by Leisure and Cultural Services Department",
+		price:"$123",
+	},
+
+	{
+		id: 2,
+		title:  "Fest of Belles and Queens: Actresses European and Hollywood",
+		venue: "75010017",
+		data: "11 December 2022 (Sun) 11:30am 13 January 2023 (Fri) 8pm",
+		time: "84 mins",
+		description:"years...",
+		presenter:"Presented by Leisure and Cultural Services Department",
+		price:"$999",
+	},
+];
